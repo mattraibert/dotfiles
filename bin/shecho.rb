a57b569fb75c3ac7a1a2720cd3a4854901e0f026
Matt J Raibert <team-nyt@cyruslists.com>
2013-08-08 12:39:38 -0400

    fix branch_ancestry bug

--- bin/shecho.rb
+++ bin/shecho.rb
@@ -1,4 +1,4 @@
-def shecho(cmd, echo_mode = "echo and execute")
-  puts "$ #{cmd}" if(/echo/ =~ echo_mode)
-  `#{cmd}` if(/execute/ =~ echo_mode)
+def shecho(cmd, options = {:echo => false, :execute => true})
+  puts "$ #{cmd}" if(options[:echo])
+  `#{cmd}` if(options[:execute])
 end
