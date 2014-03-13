800c779ff67bf1b5480ec309fdf31cb31277ee4f
Cyrus Team <cyrusteam@cyruslists.com>
2014-03-13 16:00:33 -0400

    auto-save/revert

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -17,7 +17,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(auto-save-interval 20)
+ '(auto-save-timeout 3)
+ '(auto-save-visited-file-name t)
  '(custom-safe-themes (quote ("0ebe0307942b6e159ab794f90a074935a18c3c688b526a2035d14db1214cf69c" "a774c5551bc56d7a9c362dca4d73a374582caedb110c201a09b410c0ebbb5e70" "c7359bd375132044fe993562dfa736ae79efc620f68bab36bd686430c980df1c" "bf648fd77561aae6722f3d53965a9eb29b08658ed045207fe32ffed90433eb52" "33c5a452a4095f7e4f6746b66f322ef6da0e770b76c0ed98a438e76c497040bb" default)))
+ '(global-auto-revert-mode t)
  '(global-linum-mode t)
  '(ido-mode (quote both) nil (ido))
  '(inhibit-startup-screen t)
@@ -30,4 +34,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:background nil)))))
