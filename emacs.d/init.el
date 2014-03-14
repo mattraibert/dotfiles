f282b40fdc4c3175695789d68c3a0cf19ec5d9ec
Cyrus Team <cyrusteam@cyruslists.com>
2014-03-14 13:32:54 -0400

    fix ido

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -13,7 +13,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-enabled-themes (quote (adwaita)))
- '(custom-safe-themes (quote ("90b5269aefee2c5f4029a6a039fb53803725af6f5c96036dee5dc029ff4dff60" "0ebe0307942b6e159ab794f90a074935a18c3c688b526a2035d14db1214cf69c" "a774c5551bc56d7a9c362dca4d73a374582caedb110c201a09b410c0ebbb5e70" "c7359bd375132044fe993562dfa736ae79efc620f68bab36bd686430c980df1c" "bf648fd77561aae6722f3d53965a9eb29b08658ed045207fe32ffed90433eb52" "33c5a452a4095f7e4f6746b66f322ef6da0e770b76c0ed98a438e76c497040bb" default))))
+ '(custom-safe-themes (quote ("90b5269aefee2c5f4029a6a039fb53803725af6f5c96036dee5dc029ff4dff60" "0ebe0307942b6e159ab794f90a074935a18c3c688b526a2035d14db1214cf69c" "a774c5551bc56d7a9c362dca4d73a374582caedb110c201a09b410c0ebbb5e70" "c7359bd375132044fe993562dfa736ae79efc620f68bab36bd686430c980df1c" "bf648fd77561aae6722f3d53965a9eb29b08658ed045207fe32ffed90433eb52" "33c5a452a4095f7e4f6746b66f322ef6da0e770b76c0ed98a438e76c497040bb" default)))
+ '(ido-mode (quote both) nil (ido)))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -44,12 +45,12 @@
 ;; ido/smex/recentf
 (setq ido-enable-flex-matching t)
 (setq ido-everywhere t)
-(setq ido-mode 1)
+;;(setq ido-mode 1)
 (setq flx-ido-mode 1) 
 (setq ido-use-filename-at-point (quote guess))
 
 (autoload 'smex "smex")
-;;(global-set-key (kbd "M-x") 'smex)
+(global-set-key (kbd "M-x") 'smex)
 
 (require 'recentf)
 (recentf-mode t)
