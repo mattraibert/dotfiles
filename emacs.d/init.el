02f1af985fef695ca216f8306275ed7ff851dece
Matt J Raibert <mattraibert@gmail.com>
2014-03-26 09:56:37 -0400

    disable emacs lockfiles

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -15,6 +15,7 @@
  '(auto-save-interval 20)
  '(auto-save-timeout 3)
  '(auto-save-visited-file-name t)
+ '(create-lockfiles nil)
  '(custom-enabled-themes (quote (adwaita)))
  '(custom-safe-themes (quote ("90b5269aefee2c5f4029a6a039fb53803725af6f5c96036dee5dc029ff4dff60" "0ebe0307942b6e159ab794f90a074935a18c3c688b526a2035d14db1214cf69c" "a774c5551bc56d7a9c362dca4d73a374582caedb110c201a09b410c0ebbb5e70" "c7359bd375132044fe993562dfa736ae79efc620f68bab36bd686430c980df1c" "bf648fd77561aae6722f3d53965a9eb29b08658ed045207fe32ffed90433eb52" "33c5a452a4095f7e4f6746b66f322ef6da0e770b76c0ed98a438e76c497040bb" default)))
  '(display-time-day-and-date t)
