817e70e66a4eb051ab6ec7c01b5e2fa24e5e8993
Matt J Raibert <mattraibert@gmail.com>
2014-04-01 09:22:37 -0400

    ff in repo

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -59,3 +59,5 @@
 
 (add-hook 'haskell-mode-hook 'flycheck-mode)
 (add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
+(require 'find-file-in-repository)
+(global-set-key (kbd "C-x C-f") 'find-file-in-repository)
