d9e83b0380741afbbd48035b0ea8277b499d0731
Matt J Raibert <mattraibert@positiondev.com>
2016-12-11 15:44:38 -0500

    rudimentary .emacs.d

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -14,21 +14,15 @@
   (require 'use-package))
 
 (load "~/.emacs.d/config/global.el")
-; (load "~/.emacs.d/config/autosave.el")
-(load "~/.emacs.d/config/languages/haskell.el")
-; (load "~/.emacs.d/config/languages/ocaml.el")
-(load "~/.emacs.d/config/languages/ruby.el")
-(load "~/.emacs.d/config/languages/web.el")
-(load "~/.emacs.d/config/languages/proof.el")
+(load "~/.emacs.d/config/autosave.el")
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(global-linum-mode t)
- '(helm-ff-fuzzy-matching t)
- '(helm-ff-guess-ffap-filenames nil)
- '(helm-ff-guess-ffap-urls nil)
+ '(package-selected-packages (quote (use-package)))
  '(safe-local-variable-values (quote ((encoding . utf-8)))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
