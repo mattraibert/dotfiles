a27bfc70ead00613e6868a49e470c9d7aa18a7d5
Matt J Raibert <mattraibert@positiondev.com>
2017-05-19 08:26:48 -0400

    fix auto-revert

--- emacs.d/config/autosave.el
+++ emacs.d/config/autosave.el
@@ -1,7 +1,9 @@
 (setq auto-save-interval 20)
 (setq auto-save-timeout 1)
-(setq auto-revert-interval 1)
 (setq auto-save-visited-file-name t)
 (setq create-lockfiles nil)
-(setq global-auto-revert-mode t)
 (setq make-backup-files nil)
+
+;auto revert
+(global-auto-revert-mode t)
+(setq auto-revert-interval 0.1)
