d27adb46946b8149132e1d3df2328cf733688e23
Matt J Raibert <mattraibert@positiondev.com>
2023-02-18 06:55:59 -0500

    Better output from config setup script for existing links

--- config.rb
+++ config.rb
@@ -5,13 +5,15 @@ require_relative './bin/shecho'
 pwd = Dir.pwd
 home = ENV['HOME']
 dir = Dir.glob "*"
-['bin', 'emacs.d', 'gitconfig', 'gitignore', 'bashrc', 'bash_profile', 'gitattributes', 'gemrc'].each do |dotfile|
+%w[bin emacs.d gitconfig gitignore bashrc bash_profile gitattributes gemrc].each do |dotfile|
   sourcefile = "#{pwd}/#{dotfile}"
   destfile = "#{home}/.#{dotfile}"
   if dir.include? dotfile
-    unless(File.symlink? destfile)
-      shecho("mv #{destfile} #{destfile}.bak") if(File.exist? destfile)
+    if !File.symlink? destfile
+      shecho("mv #{destfile} #{destfile}.bak") if (File.exist? destfile)
       shecho("ln -sf #{sourcefile} #{destfile}")
+    else
+      puts "skipping existing link: #{destfile} -> #{File.readlink destfile}"
     end
   else
     puts "could not find #{dotfile}, run it from the dotfiles directory"
